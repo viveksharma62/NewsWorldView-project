b252fd0218c640016367564ce529bf829e34e07c
viveksharma62 <viveksharma28402@gmail.com>
2025-07-21 23:06:33 +0530

    update api

--- src/components/News.js
+++ src/components/News.js
@@ -37,7 +37,8 @@ export class News extends Component {
 
   async updateNews(){
     this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=43fd61d964d74737bcef1c3bb3f4af6c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=43fd61d964d74737bcef1c3bb3f4af6c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+
     this.setState({loading:true}); 
     let data =await fetch(url);
     this.props.setProgress(30);
